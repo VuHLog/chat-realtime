2e93d18362655bc147c35957b510b5e30878350b
LHPC <vuhlong1412@gmail.com>
2024-09-10 14:55:29 +0700

    format time sent message

--- chat-service/src/main/java/com/vuhlog/chat/Utils/TimestampUtil.java
+++ chat-service/src/main/java/com/vuhlog/chat/Utils/TimestampUtil.java
@@ -3,9 +3,23 @@ package com.vuhlog.chat.Utils;
 import java.sql.Timestamp;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.Instant;
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.function.Function;
 
 public class TimestampUtil {
+//    Map<Long, Function<Timestamp, String>> strategyMap = new LinkedHashMap<>();
+
+//    public TimestampUtil() {
+//        strategyMap.put(60L, this::formatInSeconds); // time < 1 minute
+//        strategyMap.put(3600L, this::formatInMinutes); // time < 1 hour
+//        strategyMap.put(86400L, this::formatInHours); //time < 1 day
+//        strategyMap.put(Long.MAX_VALUE, this::formatInDate);
+//    }
+
     public static Timestamp stringToTimestamp(String dateString) {
         try {
             SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
@@ -15,12 +29,50 @@ public class TimestampUtil {
         }
     }
 
-    public static String timestampToString(Timestamp timestamp) {
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    private String formatInSeconds(Timestamp timestamp) {
+        long elapseSeconds = ChronoUnit.SECONDS.between(timestamp.toInstant(), Instant.now());
+        return elapseSeconds + " giây trước";
+    }
+
+    private String formatInMinutes(Timestamp timestamp) {
+        long elapseMinutes = ChronoUnit.MINUTES.between(timestamp.toInstant(), Instant.now());
+        return elapseMinutes + " phút trước";
+    }
+
+    private String formatInHours(Timestamp timestamp) {
+        long elapseHours = ChronoUnit.HOURS.between(timestamp.toInstant(), Instant.now());
+        return elapseHours + " giờ trước";
+    }
+
+    private String formatInDate(Timestamp timestamp) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
         return dateFormat.format(timestamp);
     }
 
-    public static Timestamp getTimeNow(){
+    public static String timestampToString(Timestamp timestamp) {
+        long elapseSeconds = ChronoUnit.SECONDS.between(timestamp.toInstant(), Instant.now());
+//
+//        var strategy = strategyMap.entrySet().stream()
+//                .filter(longFunctionEntry -> elapseSeconds < longFunctionEntry.getKey()).findFirst().get();
+//
+//        return strategy.getValue().apply(timestamp);
+
+
+        if (elapseSeconds < 60) {
+            return elapseSeconds + " giây trước";
+        } else if (elapseSeconds < 3600) {
+            long elapseMinutes = ChronoUnit.MINUTES.between(timestamp.toInstant(), Instant.now());
+            return elapseMinutes + " phút trước";
+        } else if (elapseSeconds < 86400) {
+            long elapseHours = ChronoUnit.HOURS.between(timestamp.toInstant(), Instant.now());
+            return elapseHours + " giờ trước";
+        }
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+        return dateFormat.format(timestamp);
+    }
+
+    public static Timestamp getTimeNow() {
         return Timestamp.valueOf(LocalDateTime.now());
     }
 }
--- chat-service/src/main/java/com/vuhlog/chat/controller/ChatController.java
+++ chat-service/src/main/java/com/vuhlog/chat/controller/ChatController.java
@@ -1,5 +1,6 @@
 package com.vuhlog.chat.controller;
 
+import com.vuhlog.chat.Utils.TimestampUtil;
 import com.vuhlog.chat.dto.Request.MessagesRequest;
 import com.vuhlog.chat.publisher.RabbitMQJsonProducer;
 import com.vuhlog.chat.service.MessagesService;
@@ -7,6 +8,8 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.Timestamp;
 
 @RestController
 public class ChatController {
@@ -22,8 +25,14 @@ public class ChatController {
     @MessageMapping("/sendMessage")
     public void sendMessage(@Payload MessagesRequest request) throws Exception {
         String conversationId = request.getConversationId();
+
+        String timeSent = request.getTimeSent();
+        Timestamp timestamp =TimestampUtil.stringToTimestamp(timeSent);
+        request.setTimeSent(TimestampUtil.timestampToString(timestamp));
         String destination = "/topic/conversations/" + conversationId;
         messagingTemplate.convertAndSend(destination, request);
+
+        request.setTimeSent(timeSent);
         rabbitMQJsonProducer.sendJsonMessage(request);
     }
 
